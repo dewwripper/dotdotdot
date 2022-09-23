d26d9878b27d62daa1d94716796fb59a7174981a
dewwripper <deww.ripper@gmail.com>
2022-09-24 00:16:51 +0700

    Update config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+  nvm use lts
+	alias vim=nvim
+  alias ddl=dynasty-dl
+  alias lsa="ls -la"
+  alias gst="git status"
+  alias gbr="git branch --all"
+  alias gcp="git add . && git commit --amend && git push -f"
+    # Commands to run in interactive sessions can go here
+end
