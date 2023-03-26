1181b324bf6427ed792e324d02cd046aef6cba47
dewwripper <deww.ripper@gmail.com>
2023-03-26 23:38:52 +0700

    new commit for dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,5 @@
-require('base')
-require('highlights')
-require('maps')
-require('plugins')
-
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-
-local is_mac = has "macunix"
-local is_win = has "win32"
-
-if is_mac then
-  require('macos')
-end
-
-if is_win then
-  require('windows')
-end
+require("dt.base") -- General Settings
+require("dt.highlights") -- Colourscheme and other highlights
+require("dt.maps") -- Keymaps
+require("dt.plugins") -- Plugins
+require("dt.bootstrap") -- Packer Auto-Installer
